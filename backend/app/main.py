b33193f3767b7d7d844e545fe31f35b808da9669
SoliGabiAnn <solich.gabriela@gmail.com>
2025-11-26 17:20:41 +0100

    everything works

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.admin_routes import adminRouter
+from backend.app.api.admin_routes import adminRouter
 
 app = FastAPI()
 
