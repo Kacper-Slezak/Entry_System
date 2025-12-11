f2e8a4f2dccae3f9ad9e45608973d300304d5546
Kacper <k.slezak2207@gmail.com>
2025-12-11 13:42:48 +0100

    feat: Working email sender

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.app.api.admin_routes import adminRouter
-from backend.app.db.session import engine
-from backend.app.db import models
+from app.api.admin_routes import adminRouter
+from app.db.session import engine
+from app.db import models
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,6 +1,12 @@
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+from fastapi import UploadFile
 import qrcode
 from qrcode.image.styles.moduledrawers.pil import RoundedModuleDrawer
 from io import BytesIO
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def generate_qr_code(data: str) -> BytesIO:
     """Generate a QR code image in PNG format from the given data string.
@@ -27,3 +33,35 @@ def generate_qr_code(data: str) -> BytesIO:
     img_byte_arr.seek(0)
 
     return img_byte_arr
+
+conf = ConnectionConfig(
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME", ""),
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", ""),
+    MAIL_FROM = os.getenv("MAIL_FROM"),
+    MAIL_PORT = int(os.getenv("MAIL_PORT", 587)),
+    MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com"),
+    MAIL_STARTTLS = os.getenv("MAIL_STARTTLS", "True"),
+    MAIL_SSL_TLS = os.getenv("MAIL_SSL_TLS", "False"),
+    USE_CREDENTIALS = os.getenv("USE_CREDENTIALS", "True"),
+    VALIDATE_CERTS = os.getenv("VALIDATE_CERTS", "True")
+)
+
+async def send_qr_code_via_email(email: str, qr_code_stream: BytesIO):
+    """
+    Docstring for send_qr_code_via_email
+    
+    :param email: Description
+    :type email: str
+    :param qr_code_stream: Description
+    :type qr_code_stream: BytesIO
+    """
+    qr_code_file = UploadFile(file=qr_code_stream, filename="qrcode.png")
+    message = MessageSchema(
+        subject="Witamy w firmie! Oto Twój kod QR",
+        recipients=[email],
+        body="W załączniku znajduje się Twój kod QR.",
+        subtype="html", 
+        attachments=[qr_code_file]
+    )
+    fm = FastMail(conf)
+    await fm.send_message(message)
