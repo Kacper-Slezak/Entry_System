74c42afefa26e4e2240bd0da4c16df64dc391d8c
Martynka123-code <martyna.peukert@gmail.com>
2025-12-15 19:25:10 +0100

    Default admin creation.

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,9 +5,33 @@ from app.db.session import engine
 from app.db import models
 
 from dotenv import load_dotenv
+
+from backend.app.core.security import get_password_hash
+from backend.app.db.session import SessionLocal
+
 load_dotenv()
 
 models.Base.metadata.create_all(bind=engine)
+
+def create_default_admin():
+    db = SessionLocal()
+    try:
+        admin = db.query(models.Admin).first()
+        if not admin:
+            print("INFO: Tworzenie domyślnego konta administratora...")
+            default_admin = models.Admin(
+                username = "admin",
+                hashed_password = get_password_hash("admin123")
+            )
+            db.add(default_admin)
+            db.commit()
+            print("INFO: Utworzono konto administratora: admin / admin123")
+    finally:
+        db.close()
+
+create_default_admin()
+
+
 app = FastAPI()
 
 app.add_middleware(
